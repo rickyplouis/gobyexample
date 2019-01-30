51af477bfc0112f13f4a65a87438f0feb10a2d82
rickyplouis <ricardoplouis@gmail.com>
2019-01-30 16:10:22 -0600

    ifelse

---
+++ if-else.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+  imReal := true
+    if imReal {
+      fmt.Println("Im the realest")
+    }
+
+    if (false) {
+      fmt.Println("Are you?")
+    } else {
+      fmt.Println("Aren't you?")
+    }
+}
