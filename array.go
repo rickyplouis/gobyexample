848f9f696b5eb487205c45da82c5feb46d6418ec
rickyplouis <ricardoplouis@gmail.com>
2019-01-30 16:26:24 -0600

    For loop

---
+++ array.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    var ayee [5] int
+    var bay[100] int
+    kay := [5]int{1, 2, 3, 4, 5}
+
+    for i := 0; i < 100; i++ {
+      bay[i] = i
+    }
+
+    var test[10][10]int
+    for j := 0; j < 10; j++ {
+      for k := 0; k < 10; k++ {
+        test[j][k] = j + k
+      }
+    }
+    fmt.Println(ayee)
+    fmt.Println(bay)
+    fmt.Println(kay)
+    fmt.Println(test)
+}
